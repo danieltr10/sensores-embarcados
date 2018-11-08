f50a33c7921b21fe7cdf68ece6300c562fc1ddc8
Daniel <daniel@livup.com.br>
2018-11-08 16:07:20 -0200

    raspberry gprs code

--- ESP-mqttConnection.ino
+++ ESP-mqttConnection.ino
@@ -1,67 +1,67 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
- 
+
 const char* ssid = "iPhone de Daniel";
 const char* password =  "dani12345";
-const char* mqttServer = "m15.cloudmqtt.com";
-const int mqttPort = 12018;
-const char* mqttUser = "wzthdctv";
-const char* mqttPassword = "oVW54jmodqa6";
- 
+const char* mqttServer = "172.24.1.1";
+const int mqttPort = 1883;
+const char* mqttUser = "username";
+const char* mqttPassword = "grupo6";
+
 WiFiClient espClient;
 PubSubClient client(espClient);
- 
+
 void setup() {
- 
+
   Serial.begin(115200);
- 
+
   WiFi.begin(ssid, password);
- 
+
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.println("Connecting to WiFi..");
   }
   Serial.println("Connected to the WiFi network");
- 
+
   client.setServer(mqttServer, mqttPort);
   client.setCallback(callback);
- 
+
   while (!client.connected()) {
     Serial.println("Connecting to MQTT...");
- 
+
     if (client.connect("ESP8266Client", mqttUser, mqttPassword )) {
- 
-      Serial.println("connected");  
- 
+
+      Serial.println("connected");
+
     } else {
- 
+
       Serial.print("failed with state ");
       Serial.print(client.state());
       delay(2000);
- 
+
     }
   }
- 
+
   client.publish("esp/test", "Hello from ESP8266");
   client.subscribe("esp/test");
- 
+
 }
- 
+
 void callback(char* topic, byte* payload, unsigned int length) {
- 
+
   Serial.print("Message arrived in topic: ");
   Serial.println(topic);
- 
+
   Serial.print("Message:");
   for (int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
   }
- 
+
   Serial.println();
   Serial.println("-----------------------");
- 
+
 }
- 
+
 void loop() {
   client.loop();
 }
